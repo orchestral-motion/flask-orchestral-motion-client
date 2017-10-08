a1c7748a6c3c47344851d76eef873a26a8946c60
palewire <ben.welsh@gmail.com>
2017-10-07 21:09:23 -0700

    10 at a time

--- templates/index.js
+++ templates/index.js
@@ -41,12 +41,13 @@ d3.csv("/static/data/test.csv?foo=baz", function(d) {
 
 var ws_path =  "wss://murmuring-dawn-85833.herokuapp.com/stream/";
 var table = $("#table tbody");
-var form = $("form");
 var template = _.template($("#table-row-template").html());
+
 console.log("Connecting to " + ws_path);
 var webSocketBridge = new channels.WebSocketBridge();
 webSocketBridge.connect(ws_path);
 webSocketBridge.listen();
+
 webSocketBridge.demultiplex('position', function(payload, streamName) {
     console.log(payload);
     // Handle different actions
@@ -55,11 +56,12 @@ webSocketBridge.demultiplex('position', function(payload, streamName) {
         table.prepend(tr);
     }
 });
-form.submit(function (e) {
+
+$("form#single").submit(function (e) {
     e.preventDefault();
-    var x = $("#x", form).val() || 1;
-    var y = $("#y", form).val() || 2;
-    var z = $("#z", form).val() || 3;
+    var x = $("#x", $(this)).val() || 1;
+    var y = $("#y", $(this)).val() || 2;
+    var z = $("#z", $(this)).val() || 3;
     var data = {
         x: x,
         y: y,
@@ -71,6 +73,25 @@ form.submit(function (e) {
         "data": data
     });
 });
+
+$("form#many").submit(function (e) {
+    e.preventDefault();
+    _.each(_.range(10), function(i) {
+        var num = Math.floor(Math.random() * 10);
+        var data = {
+            x: num,
+            y: num,
+            z: num
+        };
+        console.log(data)
+        webSocketBridge.stream('position').send({
+            "pk": 1,
+            "action": "create",
+            "data": data
+        });
+    })
+});
+
 // Helpful debugging
 webSocketBridge.socket.addEventListener('open', function() { console.log("Connected to notification socket"); });
 webSocketBridge.socket.addEventListener('close', function() { console.log("Disconnected to notification socket"); });
