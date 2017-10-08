0a3ea86d28fb177dcd15bd38386cba952b7ccbb0
palewire <ben.welsh@gmail.com>
2017-10-07 20:59:25 -0700

    Dummy d3 chart with fake data

--- templates/index.js
+++ templates/index.js
@@ -1,3 +1,44 @@
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ");
+
+var x = d3.scaleTime()
+    .rangeRound([0, width]);
+
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+var line = d3.line()
+    .x(function(d) { return x(d.timestamp); })
+    .y(function(d) { return y(d.x); });
+
+d3.csv("/static/data/test.csv?foo=baz", function(d) {
+  d.timestamp = parseTime(d.timestamp);
+  d.x = +d.x;
+  d.y = +d.y;
+  d.z = +d.z;
+  return d;
+}, function(error, data) {
+  if (error) throw error;
+  console.log(data);
+  x.domain(d3.extent(data, function(d) { return d.timestamp; }));
+  y.domain(d3.extent(data, function(d) { return d.x; }));
+
+  g.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+});
+
+
 var ws_path =  "wss://murmuring-dawn-85833.herokuapp.com/stream/";
 var table = $("#table tbody");
 var form = $("form");
